93ec3a0adf9ab8531e886982ed10ab8720dd03f3
emaGET <emanuelepini@getacademy.no>
2026-03-12 13:12:01 +0100

    controller fungerer (kanskje)

--- js/controller.js
+++ js/controller.js
@@ -5,16 +5,43 @@
 // 
 // skrive i input/update
 
-
-function addTocList() {
-
-
-    // push
+function selectItem(id) {
+    model.viewState.selectedItemId = id;
+    updateView();
 }
 
-function revomefromlist() {
+function addToList() {
+    if (model.viewState.itemName.trim() === "") return;
 
-    // filter ved sletting
+    let itemNumber = Number(model.viewState.itemNumber);
+    if (isNaN(itemNumber) || itemNumber <= 0) return;
 
+    let newItem = {
+        id: model.viewState.nextItemId,
+        text: model.viewState.itemName.trim(),
+        number: itemNumber,
+    };
+
+    model.data.shoppingList.push(newItem);
+    model.viewState.nextItemId++;
+    model.viewState.itemName = "";
+    model.viewState.itemNumber = "";
+    updateView();
 }
 
+function revomefromlist(id) {
+    let index = -1;
+
+    for (let i = 0; i < model.data.shoppingList.length; i++) {
+        if (model.data.shoppingList[i].id === id) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index === -1) return;
+
+    model.data.shoppingList.splice(index, 1);
+    model.viewState.selectedItemId = null;
+    updateView();
+}
